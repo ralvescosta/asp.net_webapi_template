f5be5f00d12f22496b18e903903934d2dccedf00
Rafael Alves <rafael.rac.mg@gmail.com>
2021-04-16 07:13:04 -0300

    feat: integration with ElmahIO

--- AspNet.Manage.StatusCode.Data/UserRepository.cs
+++ AspNet.Manage.StatusCode.Data/UserRepository.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using AspNet.Manage.StatusCode.Application;
 using AspNet.Manage.StatusCode.Domain;
+using Microsoft.Extensions.Logging;
 using OneOf;
 
 namespace AspNet.Manage.StatusCode.Data
 {
     public class UserRepository : IUserRepository
     {
+        private readonly ILogger<UserRepository> _logger;
+        public UserRepository(ILogger<UserRepository> logger)
+        {
+            _logger = logger;
+        }
         public OneOf<User, InternalErrorException> Create(User user)
         {
-            return new InternalErrorException("Ops!");
+            var ex = new InternalErrorException("Ops!");
+            _logger.LogError(ex, "Try to create an user");
+            return ex;
         }
     }
 }
--- AspNet.Manage.StatusCode.WebApi/Program.cs
+++ AspNet.Manage.StatusCode.WebApi/Program.cs
@@ -1,5 +1,8 @@
+using System;
+using Elmah.Io.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AspNet.Manage.StatusCode.WebApi
 {
@@ -12,6 +15,15 @@ namespace AspNet.Manage.StatusCode.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((ctx, logging) => 
+                {
+                    logging.AddElmahIo(options => 
+                    {
+                        options.ApiKey = "eb4044a8b7954580aabb8bf09cd5b0c0";
+                        options.LogId = new Guid("6aba9191-d1d6-48d5-a3b1-4314c642a3dd");
+                    });
+                    logging.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
